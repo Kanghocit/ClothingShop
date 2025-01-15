ee71ded78c37fe4d852daf262ec30374532b5131
Kanghocit <Ankhangit06@gmail.com>
2025-01-15 15:07:59 +0700

    updateLoveProduct

--- fe/src/Components/Header/LoveProducts.jsx
+++ fe/src/Components/Header/LoveProducts.jsx
@@ -1,8 +1,9 @@
-import { Badge } from "@mui/material";
+import { Badge, IconButton } from "@mui/material";
 import Drawer from "@mui/material/Drawer";
 import * as React from "react";
 import { useState } from "react";
 import { CiHeart } from "react-icons/ci";
+import { IoClose } from "react-icons/io5";
 
 const LoveProducts = () => {
   const [open, setOpen] = useState(false);
@@ -10,18 +11,35 @@ const LoveProducts = () => {
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
+
   return (
     <>
       <Badge
         badgeContent={4}
         color="primary"
         className="badgeColor mx-2"
-        onClick={toggleDrawer(true)}
+        onClick={toggleDrawer(true)} // Mở Drawer khi click vào Badge
       >
-        <CiHeart className=" btnSearch " />
+        <CiHeart className="btnSearch" />
       </Badge>
+
       <Drawer open={open} anchor="bottom" onClose={toggleDrawer(false)}>
-        <div className="content">hihihi</div>
+        <div className="content">
+          <div className="nameLoveText row">
+            <div className="col-11">Danh mục sản phẩm yêu thích</div>
+
+            {/* Đóng Drawer */}
+
+            <IoClose
+              className="closeButton col-1 text-end"
+              onClick={toggleDrawer(false)}
+            />
+          </div>
+
+          <div className="productsLove text-center p-5">
+            Chưa có sản phẩm nào trong danh sách
+          </div>
+        </div>
       </Drawer>
     </>
   );
